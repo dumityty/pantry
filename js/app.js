399d71e84b49e88581181dbf9a855c82a536d6f4
dumityty <dumityty@yahoo.com>
2016-07-23 12:27:56 +0100

    update, delete, add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('pantryApp', [])
+angular.module('pantryApp', ['xeditable'])
   .controller('PantryController', function($scope, $http){
     var apiurl = "";
 
@@ -15,20 +15,27 @@ angular.module('pantryApp', [])
       }
     }
 
-    // fetchPantry(req);
+    fetchPantry(req);
 
-    $scope.pantry = [
-        {
-          "item": "name1",
-          "stock": 1
-        },
-        {
-          "item": "name2",
-          "stock": 2
-        }
-      ];
+    // todo: save pantry somehow locally - in cookies, etc
+    // and only refresh it on click of a refresh button
+    // any functions below should also refresh the $scope.pantry and update the view.
+    //
+    // reload scope after each function
+    // also empty the form add after saving
 
-    console.log($scope.pantry);
+    // $scope.pantry = [
+    //   {
+    //     "item": "name1",
+    //     "stock": 1
+    //   },
+    //   {
+    //     "item": "name2",
+    //     "stock": 2
+    //   }
+    // ];
+
+    // console.log($scope.pantry);
 
     function fetchPantry(req){
       $http(req).then(function(response){
@@ -59,12 +66,68 @@ angular.module('pantryApp', [])
 
       // check for errors, etc.
       $http(req).then(function(response){
-        console.log(response);
+      //   console.log(response);
         // add the new item directly to the pantry scope (to save api calls)
         // or call fetchPantry again...
       });
     }
 
+     $scope.saveItem = function(olditem, data) {
+      console.log(olditem);
+      console.log(data);
+
+      var req = {
+        method: 'POST',
+        url: apiurl,
+        data: {
+          "operation": "update",
+          "tableName": "Pantry",
+          "payload": {
+            "Key": {
+                "item": olditem.item
+            },
+            "UpdateExpression": "set item = :i, stock = :s",
+            "ExpressionAttributeValues": {
+              ":i": data.item,
+              ":s": data.stock
+            }
+          }
+        }
+      }
+
+       // check for errors, etc.
+      $http(req).then(function(response){
+        console.log(response);
+        // update item in $scope.pantry
+        // or call fetchPantry again...
+      });
+
+     }
+
+    $scope.removeItem = function(data) {
+      console.log(data);
+
+      var req = {
+        method: 'POST',
+        url: apiurl,
+        data: {
+          "operation": "delete",
+          "tableName": "Pantry",
+          "payload": {
+            "Key": {
+              "item": data
+            }
+          }
+        }
+      }
+
+       // check for errors, etc.
+      $http(req).then(function(response){
+        // console.log(response);
+        // remove item from $scope.pantry
+        // or call fetchPantry again...
+      });
+    }
 
 
 
